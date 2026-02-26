9473cbb9d01cc3e220e2f32d414c2af86948cdb3
KEERTHAN KUMAR B M <keerthankumarbm@gmail.com>
2026-02-27 02:23:31 +0530

    Removed debug mode for production

--- app.py
+++ app.py
@@ -167,6 +167,4 @@ def admin():
 # ------------------ RUN ------------------
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+    app.run()
